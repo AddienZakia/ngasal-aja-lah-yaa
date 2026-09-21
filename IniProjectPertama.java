4632aad7b6b2c9dbe2127fa8f1741a4746edd36d
Addien Zakia <91828556+AddienZakia@users.noreply.github.com>
2026-09-21 19:50:19 +0700

    Add files via upload

---
+++ IniProjectPertama.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package iniprojectpertama;
+
+/**
+ *
+ * @author MSI GAMING
+ */
+public class IniProjectPertama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // ngasall
+        System.out.println("Hello world");
+    }
+    
+}
